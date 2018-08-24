858f367d8e11a45ce7effb899909ed4882b58b71
Kandy <objectrabbit@gmail.com>
2018-08-24 16:05:43 +0800

    添加EncryptHelper 添加MD5加密方法

---
+++ Rabbit_AspNetCore_Utility/Rabbit_AspNetCore_Utility.Encrypt/EncryptHelper.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Rabbit_AspNetCore_Utility.Encrypt
+{
+    public static class EncryptHelper
+    {
+        #region MD5加密
+        /// <summary>
+        /// 生成MD5加密字符串
+        /// </summary>
+        /// <param name="original">源字符串</param>
+        /// <returns>加密字符串</returns>
+        public static string MD5EncryptByString(string original)
+        {
+            if (string.IsNullOrEmpty(original))
+            {
+                return "";
+            }
+            else
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    var byteOriginal = Encoding.Default.GetBytes(original);
+                    var md5Ret = md5.ComputeHash(byteOriginal);
+                    var md5Sber = new StringBuilder();
+                    foreach (var item in md5Ret)
+                    {
+                        md5Sber.Append(item.ToString("X2"));
+                    }
+                    return md5Sber.ToString();
+                }
+            }
+        }
+        /// <summary>
+        /// 生成MD5加密字符串
+        /// </summary>
+        /// <param name="original">byte 数组</param>
+        /// <returns>加密字符串</returns>
+        public static string MD5EncryptByBytes(byte[] original)
+        {
+            if (original == null)
+            {
+                return "";
+            }
+            else
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    var md5Ret = md5.ComputeHash(original);
+                    var md5Sber = new StringBuilder();
+                    foreach (var item in md5Ret)
+                    {
+                        md5Sber.Append(item.ToString("X2"));
+                    }
+                    return md5Sber.ToString();
+                }
+            }
+        }
+        /// <summary>
+        /// 生成MD5加密字符串
+        /// </summary>
+        /// <param name="original">字节流</param>
+        /// <returns>加密字符串</returns>
+        public static string MD5EncryptByStream(Stream original)
+        {
+            if (original == null)
+            {
+                return "";
+            }
+            else
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    var md5Ret = md5.ComputeHash(original);
+                    var md5Sber = new StringBuilder();
+                    foreach (var item in md5Ret)
+                    {
+                        md5Sber.Append(item.ToString("X2"));
+                    }
+                    return md5Sber.ToString();
+                }
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ Rabbit_AspNetCore_Utility/Rabbit_AspNetCore_Utility.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using Rabbit_AspNetCore_Utility.Encrypt;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Rabbit_AspNetCore_Utility.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(EncryptHelper.MD5EncryptByString("admin"));
+
+
+            var origional = Encoding.Default.GetBytes("Test MD5EncryptByBytes");
+
+            Console.WriteLine(EncryptHelper.MD5EncryptByBytes(origional));
+
+            var path = AppDomain.CurrentDomain.BaseDirectory + @"log\1.txt";
+            var stream = new FileStream("1.txt",FileMode.OpenOrCreate);
+            var ret = EncryptHelper.MD5EncryptByStream(stream);
+            Console.WriteLine(ret);
+            Console.ReadKey();
+        }
+    }
+}
